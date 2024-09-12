96c4898da899ec37a78595afabb536a80750eb90
brenooki <148918473+brenooki@users.noreply.github.com>
2024-09-12 10:41:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function toggleForm(type) {
         formNatural.style.display = 'none';
     }
 }
+
+document.querySelector('.styled-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name-natural').value;
+    const age = document.getElementById('age-natural').value;
+    const alergia = document.getElementById('alergia_natural').value;
+    const medicamento = document.getElementById('medicamento_natural').value;
+    const date = document.getElementById('date_natural').value;
+    const horario = document.getElementById('horario_natural').value;
+
+    // Armazenar os dados no localStorage
+    localStorage.setItem('name', name);
+    localStorage.setItem('age', age);
+    localStorage.setItem('alergia', alergia);
+    localStorage.setItem('medicamento', medicamento);
+    localStorage.setItem('date', date);
+    localStorage.setItem('horario', horario);
+
+    // Redirecionar para a página de pagamento
+    window.location.href = 'pagamento.html';
+});
